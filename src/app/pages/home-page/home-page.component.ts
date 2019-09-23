9163f17bf2bc01400247c574f7b8be083c3f6300
John Montanye <gatewayitconsulting@outlook.com>
2019-09-22 22:43:33 -0500

    Implemented Style and Functionality to Contact.
    Simplified CSS.
    Updated todo.
    Added picture of John and LinkedIn svg.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -16,16 +16,16 @@ export class HomePageComponent implements OnInit, OnDestroy {
    constructor() { }
 
   ngOnInit() {
-      var body = document.getElementsByTagName('body')[0];
+      const body = document.getElementsByTagName('body')[0];
       body.classList.add('presentation-page');
-      var navbar = document.getElementsByTagName('nav')[0];
+      const navbar = document.getElementsByTagName('nav')[0];
       navbar.classList.add('navbar-transparent');
   }
 
-  ngOnDestroy(){
-      var body = document.getElementsByTagName('body')[0];
+  ngOnDestroy() {
+      const body = document.getElementsByTagName('body')[0];
       body.classList.remove('presentation-page');
-      var navbar = document.getElementsByTagName('nav')[0];
+      const navbar = document.getElementsByTagName('nav')[0];
       navbar.classList.remove('navbar-transparent');
   }
 }
