58b4157e926af65efb7b4e161d772664c8529aea
John Montanye <gatewayitconsulting@outlook.com>
2019-11-26 13:13:03 -0600

    Updated Project Component
    Adjusted projects copy.
    Changed projects order.
    Implemented mock of new skills tags on projects. No relation to skills component or services/models.

--- src/app/models/projects/projects.ts
+++ src/app/models/projects/projects.ts
@@ -3,6 +3,14 @@ import { Project } from '../../services/projects/project';
 export const PROJECTS: Project[] = [
   {
     id: 1,
+    name: 'Simplest Cookbook',
+    image: 'assets/img/projects/simplestCookbook.jpg',
+    websiteLink: 'https://www.simplestcookbook.com/',
+    codeLink: 'https://github.com/gatewayitconsulting/simplest-cookbook-react',
+    description: 'Cookbook app with many different recipes and features to assist people cooking.'
+  },
+  {
+    id: 2,
     name: 'Gateway IT Consulting',
     image: 'assets/img/projects/gitc.jpg',
     websiteLink: 'https://www.gatewayitconsulting.com/',
@@ -10,20 +18,12 @@ export const PROJECTS: Project[] = [
     description: 'A website to help clients and users find solutions for designing, building, and maintaining websites.'
   },
   {
-    id: 2,
-    name: 'Simplest Cookbook',
-    image: 'assets/img/projects/simplestCookbook.jpg',
-    websiteLink: 'https://www.simplestcookbook.com/',
-    codeLink: 'https://github.com/gatewayitconsulting/simplest-cookbook-react',
-    description: 'Simple cookbook with many different recipes and calculators relating to cooking.'
-  },
-  {
     id: 3,
     name: 'The Palate STL',
     image: 'assets/img/projects/thePalateSTL.jpg',
     websiteLink: 'https://www.thepalatestl.com/',
     codeLink: '',
-    description: 'A local food finder web app.'
+    description: 'A local restaurant finder app for the St. Louis area.'
   },
   {
     id: 4,
@@ -31,6 +31,6 @@ export const PROJECTS: Project[] = [
     image: 'assets/img/projects/ourLadysInn.jpg',
     websiteLink: 'https://ourladysinn.org/',
     codeLink: '',
-    description: "Website for Our Lady's Inn maternity homes in STL."
+    description: "Website for Our Lady's Inn maternity homes in St. Louis."
   }
 ];
