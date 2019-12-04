de85bfafe3532ac941d92c108627d7e08a8ba9cd
John Montanye <gatewayitconsulting@outlook.com>
2019-12-04 13:29:52 -0600

    Updated Skills
    Added icon for skills.
    Switched order for skills.
    Added skills.

--- src/app/models/skills/skills.ts
+++ src/app/models/skills/skills.ts
@@ -3,121 +3,151 @@ import { Skill } from '../../services/skills/skill';
 export const SKILLS: Skill[] = [
   {
     id: 1,
-    name: 'HTML5',
-    image: 'assets/img/skills/html5.svg'
-  },
-  {
-    id: 2,
-    name: 'CSS3',
-    image: 'assets/img/skills/css3.svg'
-  },
-  {
-    id: 3,
-    name: 'SASS',
-    image: 'assets/img/skills/sass.svg'
-  },
-  {
-    id: 4,
-    name: 'Bootstrap 4',
-    image: 'assets/img/skills/bootstrap.svg'
-  },
-  {
-    id: 5,
-    name: 'JavaScript',
-    image: 'assets/img/skills/javascript.svg'
-  },
-  {
-    id: 6,
     name: 'TypeScript',
     image: 'assets/img/skills/typescript.svg'
   },
   {
-    id: 7,
-    name: 'jQuery',
-    image: 'assets/img/skills/jquery.svg'
+    id: 2,
+    name: 'JavaScript',
+    image: 'assets/img/skills/javascript.svg'
   },
   {
-    id: 8,
+    id: 3,
+    name: 'React',
+    image: 'assets/img/skills/react.svg'
+  },
+  {
+    id: 4,
     name: 'AngularJS',
     image: 'assets/img/skills/angular.svg'
   },
   {
-    id: 9,
+    id: 5,
     name: 'Angular 6 - 8',
     image: 'assets/img/skills/angular.svg'
   },
   {
-    id: 10,
+    id: 6,
     name: 'NodeJS',
     image: 'assets/img/skills/nodejs.svg'
   },
   {
+    id: 7,
+    name: 'C#',
+    image: 'assets/img/skills/csharp.svg'
+  },
+  {
+    id: 8,
+    name: '.NET',
+    image: 'assets/img/skills/dot-net.svg'
+  },
+  {
+    id: 9,
+    name: 'HTML5',
+    image: 'assets/img/skills/html5.svg'
+  },
+  {
+    id: 10,
+    name: 'CSS3',
+    image: 'assets/img/skills/css3.svg'
+  },
+  {
     id: 11,
-    name: 'PHP',
-    image: 'assets/img/skills/php.svg'
+    name: 'SASS',
+    image: 'assets/img/skills/sass.svg'
   },
   {
     id: 12,
+    name: 'jQuery',
+    image: 'assets/img/skills/jquery.svg'
+  },
+  {
+    id: 13,
+    name: 'Bootstrap 4',
+    image: 'assets/img/skills/bootstrap.svg'
+  },
+  {
+    id: 14,
+    name: 'Material Design',
+    image: 'assets/img/skills/materialdesign.svg'
+  },
+  {
+    id: 15,
+    name: 'AWS',
+    image: 'assets/img/skills/amazonaws.svg'
+  },
+  {
+    id: 16,
     name: 'mySQL',
     image: 'assets/img/skills/mysql.svg'
   },
   {
-    id: 13,
+    id: 17,
+    name: 'PostgreSQL',
+    image: 'assets/img/skills/postgresql.svg'
+  },
+  {
+    id: 18,
     name: 'MongoDB',
     image: 'assets/img/skills/mongodb.svg'
   },
   {
-    id: 14,
+    id: 19,
+    name: 'PHP',
+    image: 'assets/img/skills/php.svg'
+  },
+  {
+    id: 20,
     name: 'WordPress',
     image: 'assets/img/skills/wordpress.svg'
   },
   {
-    id: 15,
+    id: 21,
     name: 'npm',
     image: 'assets/img/skills/npm.svg'
   },
   {
-    id: 16,
+    id: 22,
     name: 'Gulp',
     image: 'assets/img/skills/gulp.svg'
   },
   {
-    id: 17,
+    id: 23,
     name: 'Git/VC',
     image: 'assets/img/skills/git.svg'
   },
   {
-    id: 18,
+    id: 24,
     name: 'Photoshop',
     image: 'assets/img/skills/adobephotoshop.svg'
   },
   {
-    id: 19,
+    id: 25,
     name: 'Sketch',
     image: 'assets/img/skills/sketch.svg'
   },
   {
-    id: 20,
+    id: 26,
     name: 'GA',
     image: 'assets/img/skills/googleanalytics.svg'
   },
   {
-    id: 21,
+    id: 27,
     name: 'JSON',
     image: 'assets/img/skills/json.svg'
   },
   {
-    id: 22,
+    id: 28,
     name: 'Agile',
     image: 'assets/img/skills/agile.png'
   },
   {
-    id: 23,
+    id: 29,
     name: 'Responsive',
     image: 'assets/img/skills/responsive.svg'
   },
   {
-    id: 24,
+    id: 30,
     name: 'UX / UI',
     image: 'assets/img/skills/ux-ui.svg'
   }
